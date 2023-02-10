a6d618b6d968b0e8e24568b27f398364f7837bf0
Nertsal <sasha.kudasov04@gmail.com>
2023-02-10 22:32:46 +0300

    Cleaner ui impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 use geng::prelude::*;
 
+mod ui;
+
 /// A wrapper for a game that implements TAS functionality:
 /// save states, slow motion, input replay.
 pub struct Tas<T: Tasable> {
     geng: Geng,
+    framebuffer_size: vec2<usize>,
     /// The game state that is manipulated.
     game: T,
     show_ui: bool,
@@ -68,6 +71,7 @@ impl<T: Tasable> Tas<T> {
     pub fn new(game: T, geng: &Geng) -> Self {
         let mut tas = Self {
             geng: geng.clone(),
+            framebuffer_size: vec2(1, 1),
             show_ui: true,
             time_scale: 1.0,
             paused: true,
@@ -154,7 +158,11 @@ impl<T: Tasable> Tas<T> {
     }
 
     fn load_savestates(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        let file = std::fs::File::open("savedstates.json")?;
+        let Ok(file) = std::fs::File::open("savedstates.json") else {
+            warn!("Failed to open savedstates.json");
+            self.saved_states = default();
+            return Ok(());
+        };
         let reader = std::io::BufReader::new(file);
         self.saved_states = serde_json::from_reader(reader)?;
         Ok(())
@@ -163,6 +171,7 @@ impl<T: Tasable> Tas<T> {
 
 impl<T: geng::State + Tasable> geng::State for Tas<T> {
     fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
+        self.framebuffer_size = framebuffer.size();
         self.game.draw(framebuffer);
     }
 
@@ -249,12 +258,35 @@ impl<T: geng::State + Tasable> geng::State for Tas<T> {
         self.queued_inputs.push(event);
     }
 
-    fn ui<'a>(&'a mut self, ui: &'a geng::ui::Controller) -> Box<dyn geng::ui::Widget + 'a> {
+    fn ui<'a>(&'a mut self, cx: &'a geng::ui::Controller) -> Box<dyn geng::ui::Widget + 'a> {
         if !self.show_ui {
-            return self.game.ui(ui);
+            return self.game.ui(cx);
         }
 
         use geng::ui::{column, *};
+
+        let framebuffer_size = self.framebuffer_size.map(|x| x as f32);
+        let text_size = framebuffer_size.y * 0.05;
+
+        let font = self.geng.default_font().clone();
+        let slider = move |name, range, value: &mut f64, text_size| {
+            ui::slider(cx, name, value, range, font.clone(), text_size)
+        };
+
+        let font = self.geng.default_font().clone();
+        let text =
+            move |text, text_size| geng::ui::Text::new(text, font.clone(), text_size, Rgba::WHITE);
+
+        macro_rules! button {
+            ($name:expr => $callback:block) => {{
+                let button = Button::new(cx, $name);
+                if button.was_clicked() {
+                    $callback
+                }
+                button
+            }};
+        }
+
         let mut load_state = None;
         let mut delete_state = None;
         let mut saved_states: Vec<_> = self
@@ -262,33 +294,19 @@ impl<T: geng::State + Tasable> geng::State for Tas<T> {
             .iter()
             .enumerate()
             .map(|(i, _)| {
-                Box::new(
-                    row![
-                        Text::new(
-                            format!("Save #{i}"),
-                            self.geng.default_font().clone(),
-                            30.0,
-                            Rgba::BLACK
-                        ),
-                        {
-                            let load_save = Button::new(ui, "Load");
-                            if load_save.was_clicked() {
-                                load_state = Some(i);
-                            }
-                            load_save
-                        }
-                        .padding_horizontal(20.0),
-                        {
-                            let delete_save = Button::new(ui, "Delete");
-                            if delete_save.was_clicked() {
-                                delete_state = Some(i);
-                            }
-                            delete_save
-                        }
-                        .padding_horizontal(20.0),
-                    ]
-                    .padding_vertical(10.0),
-                ) as Box<dyn Widget>
+                row![
+                    text(format!("Save #{i}"), text_size,),
+                    button!("Load" => {
+                        load_state = Some(i);
+                    })
+                    .padding_horizontal(20.0),
+                    button!("Delete" => {
+                            delete_state = Some(i);
+                    })
+                    .padding_horizontal(20.0),
+                ]
+                .padding_vertical(10.0)
+                .boxed()
             })
             .collect();
         if let Some(i) = delete_state {
@@ -299,73 +317,36 @@ impl<T: geng::State + Tasable> geng::State for Tas<T> {
 
         let tas_ui = stack![
             if self.paused {
-                Box::new(
-                    Text::new(
-                        "Paused",
-                        self.geng.default_font().clone(),
-                        50.0,
-                        Rgba::BLACK,
-                    )
-                    .align(vec2(1.0, 0.9)),
-                ) as Box<dyn Widget>
+                text("Paused".to_string(), text_size)
+                    .align(vec2(1.0, 0.9))
+                    .boxed()
             } else {
-                Box::new(Void)
+                Void.boxed()
             },
+            slider("Time scale", 0.0..=10.0, &mut self.time_scale, text_size).align(vec2(0.5, 1.0)),
             column![
-                {
-                    let time_slider = slider::Slider::new(ui, self.time_scale, 0.0..=10.0);
-                    if let Some(value) = time_slider.get_change() {
-                        self.time_scale = value;
-                    }
-                    time_slider
-                },
-                Text::new(
-                    format!("Time scale: {:.2}", self.time_scale),
-                    self.geng.default_font().clone(),
-                    50.0,
-                    Rgba::BLACK
-                )
-                .center()
-            ]
-            .align(vec2(0.5, 1.0)),
-            column![
-                Text::new(
-                    self.save_file.clone(),
-                    self.geng.default_font().clone(),
-                    40.0,
-                    Rgba::BLACK
-                ),
+                text(self.save_file.clone(), text_size),
                 row![
-                    {
-                        let save_run = Button::new(ui, "Save run");
-                        if save_run.was_clicked() {
-                            if let Err(err) = self.save_run(&self.save_file) {
-                                error!("Failed to save run: {err}");
-                            }
+                    button!("Save run" => {
+                        if let Err(err) = self.save_run(&self.save_file) {
+                            error!("Failed to save run: {err}");
                         }
-                        save_run
-                    },
-                    {
-                        let load_run = Button::new(ui, "Start replay");
-                        if load_run.was_clicked() {
-                            println!("The evil button");
-                            if let Err(err) = self.load_run(&self.save_file.clone()) {
-                                error!("Failed to load run: {err}");
-                            }
+                    }),
+                    button!("Start replay" => {
+                        if let Err(err) = self.load_run(&self.save_file.clone()) {
+                            error!("Failed to load run: {err}");
                         }
-                        load_run
-                    }
+                    }),
                 ]
             ]
             .align(vec2(0.0, 0.0)),
             column({
-                saved_states.push(Box::new({
-                    let new_save = Button::new(ui, "Save state");
-                    if new_save.was_clicked() {
+                saved_states.push(
+                    button!("Save state" => {
                         self.save_state();
-                    }
-                    new_save
-                }) as Box<dyn Widget>);
+                    })
+                    .boxed(),
+                );
                 saved_states
             })
             .align(vec2(1.0, 0.0))
@@ -373,145 +354,6 @@ impl<T: geng::State + Tasable> geng::State for Tas<T> {
         ]
         .uniform_padding(30.0);
 
-        Box::new(stack(vec![self.game.ui(ui), Box::new(tas_ui)]))
-    }
-}
-
-mod slider {
-    use super::*;
-    use geng::ui::*;
-
-    pub struct Slider<'a> {
-        cx: &'a Controller,
-        sense: &'a mut Sense,
-        pos: &'a mut Option<Aabb2<f64>>,
-        tick_radius: &'a mut f32,
-        value: f64,
-        range: RangeInclusive<f64>,
-        change: RefCell<&'a mut Option<f64>>,
-    }
-
-    impl<'a> Slider<'a> {
-        const ANIMATION_SPEED: f32 = 5.0;
-
-        pub fn new(cx: &'a Controller, value: f64, range: RangeInclusive<f64>) -> Self {
-            Slider {
-                cx,
-                sense: cx.get_state(),
-                tick_radius: cx.get_state(),
-                pos: cx.get_state(),
-                value,
-                range,
-                change: RefCell::new(cx.get_state()),
-            }
-        }
-
-        pub fn get_change(&self) -> Option<f64> {
-            self.change.borrow_mut().take()
-        }
-    }
-
-    impl<'a> Widget for Slider<'a> {
-        fn sense(&mut self) -> Option<&mut Sense> {
-            Some(self.sense)
-        }
-        fn update(&mut self, delta_time: f64) {
-            let target_tick_radius = if self.sense.is_hovered() || self.sense.is_captured() {
-                1.0 / 2.0
-            } else {
-                1.0 / 6.0
-            };
-            *self.tick_radius += (target_tick_radius - *self.tick_radius)
-                .clamp_abs(Self::ANIMATION_SPEED * delta_time as f32);
-        }
-        fn draw(&mut self, cx: &mut DrawContext) {
-            *self.pos = Some(cx.position);
-            let geng = cx.geng;
-            let draw_2d = geng.draw_2d_helper();
-            let position = cx.position.map(|x| x as f32);
-            let line_width = position.height() / 3.0;
-            let value_position = if self.range.end() == self.range.start() {
-                *self.tick_radius
-            } else {
-                *self.tick_radius
-                    + ((self.value - *self.range.start())
-                        / (*self.range.end() - *self.range.start())) as f32
-                        * (position.width() - line_width)
-            };
-            geng.draw_2d(
-                cx.framebuffer,
-                &geng::PixelPerfectCamera,
-                &draw_2d::Quad::new(
-                    Aabb2::from_corners(
-                        position.bottom_left()
-                            + vec2(value_position, (position.height() - line_width) / 2.0),
-                        position.top_right()
-                            - vec2(line_width / 2.0, (position.height() - line_width) / 2.0),
-                    ),
-                    cx.theme.usable_color,
-                ),
-            );
-            draw_2d.circle(
-                cx.framebuffer,
-                &geng::PixelPerfectCamera,
-                position.top_right() - vec2(line_width / 2.0, position.height() / 2.0),
-                line_width / 2.0,
-                cx.theme.usable_color,
-            );
-            geng.draw_2d(
-                cx.framebuffer,
-                &geng::PixelPerfectCamera,
-                &draw_2d::Quad::new(
-                    Aabb2::from_corners(
-                        position.bottom_left()
-                            + vec2(line_width / 2.0, (position.height() - line_width) / 2.0),
-                        position.bottom_left()
-                            + vec2(value_position, (position.height() + line_width) / 2.0),
-                    ),
-                    cx.theme.hover_color,
-                ),
-            );
-            geng.draw_2d(
-                cx.framebuffer,
-                &geng::PixelPerfectCamera,
-                &draw_2d::Ellipse::circle(
-                    position.bottom_left() + vec2(line_width / 2.0, position.height() / 2.0),
-                    line_width / 2.0,
-                    cx.theme.hover_color,
-                ),
-            );
-            draw_2d.circle(
-                cx.framebuffer,
-                &geng::PixelPerfectCamera,
-                position.bottom_left() + vec2(value_position, position.height() / 2.0),
-                *self.tick_radius * position.height(),
-                cx.theme.hover_color,
-            );
-        }
-        fn handle_event(&mut self, event: &geng::Event) {
-            let aabb = match *self.pos {
-                Some(pos) => pos,
-                None => return,
-            };
-            if self.sense.is_captured() {
-                if let geng::Event::MouseDown { position, .. }
-                | geng::Event::MouseMove { position, .. } = &event
-                {
-                    let position = position.x - aabb.min.x;
-                    let new_value = *self.range.start()
-                        + ((position - aabb.height() / 6.0) / (aabb.width() - aabb.height() / 3.0))
-                            .clamp(0.0, 1.0)
-                            * (*self.range.end() - *self.range.start());
-                    **self.change.borrow_mut() = Some(new_value);
-                }
-            }
-        }
-
-        fn calc_constraints(&mut self, _children: &ConstraintsContext) -> Constraints {
-            Constraints {
-                min_size: vec2(1.0, 1.0) * self.cx.theme().text_size as f64,
-                flex: vec2(1.0, 0.0),
-            }
-        }
+        Box::new(stack(vec![self.game.ui(cx), Box::new(tas_ui)]))
     }
 }
